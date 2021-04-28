212a0654d221742cc7cbb837620b66140c65cac4
Stone Chen <thestonechen@gmail.com>
2021-04-28 11:33:36 -0400

    Firebase setup

---
+++ Pods/Target Support Files/Pods-Transcribe/Pods-Transcribe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Transcribe : NSObject
+@end
+@implementation PodsDummy_Pods_Transcribe
+@end
---
+++ Transcribe/TranscribeApp.swift
@@ -0,0 +1,21 @@
+//
+//  TranscribeApp.swift
+//  Transcribe
+//
+//  Created by Stone Chen on 4/28/21.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct TranscribeApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
